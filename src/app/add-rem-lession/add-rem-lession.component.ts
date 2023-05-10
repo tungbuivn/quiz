bb3e5af0685dc521c8c751fd550970a397b1afb6
tungbui <tungbuivn@gmail.com>
2023-05-10 17:11:32 +0700

    .

--- src/app/add-rem-lession/add-rem-lession.component.ts
+++ src/app/add-rem-lession/add-rem-lession.component.ts
@@ -27,7 +27,7 @@ export class AddRemLessionComponent extends AddLessionComponent {
     // num from 2->9
     var unit = this.shuffle([2, 3, 4, 5, 6, 7, 8, 9])[0];
     // unit 2 inrange 11..urange
-    var unit2 = this.shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9].filter(num => num + unit >= 11))[0] + parseInt(Math.random() * 1234 + "") % (urange - lrange) + lrange;
+    var unit2 = this.shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9].filter(num => num + unit >= 11))[0] + parseInt(Math.random() * 1234 + "") % (urange - lrange) + lrange;
     // var unit2 = parseInt(Math.random() * 1234 + "") % (urange - 11) + 11;
     // 40-10->30
     this.firstNumber = unit2;
